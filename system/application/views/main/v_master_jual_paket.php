15e154b088e180c9e7d5d180530765bb46bab5d6
hypolution <freddy@giovsolution.com>
2014-01-15 14:58:11 +0700

    test commit by fred

--- system/application/views/main/v_master_jual_paket.php
+++ system/application/views/main/v_master_jual_paket.php
@@ -1,6 +1,6 @@
 <?
 /* 	
-	GIOV Solution - Keep IT Simple
+	GIOV Solution - Keep IT Simple YEAH!!
 */
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
