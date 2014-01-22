396e0592661198159025063e1625ec547aea69f9
hypolution <freddy@giovsolution.com>
2014-01-22 09:28:10 +0700

    system logs aja

---
+++ system/logs/log-2014-01-21.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-01-21 10:56:08 --> The session cookie data did not match what was expected. This could be a possible hacking attempt.
+ERROR - 2014-01-21 10:56:09 --> Severity: Notice  --> Undefined index:  mode_edit /Applications/MAMP/htdocs/babybright/system/application/controllers/c_appointment.php 198
+ERROR - 2014-01-21 10:56:24 --> Severity: Notice  --> Undefined index:  mode_edit /Applications/MAMP/htdocs/babybright/system/application/controllers/c_appointment.php 198
+ERROR - 2014-01-21 10:56:24 --> Severity: Notice  --> Undefined index:  mode_edit /Applications/MAMP/htdocs/babybright/system/application/controllers/c_appointment.php 198
